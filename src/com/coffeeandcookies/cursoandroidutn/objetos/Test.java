b79479c2a9225a7c9ebce0a65bf0434cd69a5528
Nicolas Graña <nicolas.grana.13@gmail.com>
2019-02-10 17:49:39 -0300

    no message

--- src/com/coffeeandcookies/cursoandroidutn/objetos/Test.java
+++ src/com/coffeeandcookies/cursoandroidutn/objetos/Test.java
@@ -3,6 +3,7 @@ package com.coffeeandcookies.cursoandroidutn.objetos;
 public class Test{
 
     public static final String USER = "user";
+    public static final String PASS = "pass";
     public static final String NACIONALITY = "nationality";
 
 }
---
+++ src/com/coffeeandcookies/cursoandroidutn/objetos/Test2.java
@@ -0,0 +1,7 @@
+package com.coffeeandcookies.cursoandroidutn.objetos;
+
+public class Test2{
+
+    public static final String USER = "user";
+
+}
